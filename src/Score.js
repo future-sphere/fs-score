e8d9ea8568ed3a791d37317e16f0cabf496af548
Xu Chu <chuxu200328@gmail.com>
2020-03-20 20:40:18 -0400

    add alert

--- src/Score.js
+++ src/Score.js
@@ -53,6 +53,9 @@ function Score() {
 
   return (
     <div>
+      <div className="alert">
+      受疫情(COVID-19)影响，为了大家的健康和安全考虑，2020年3月21日起所有课程改为网课，直到疫情结束再另行通知
+      </div>
       <div class="links">
         <div>
           <img
